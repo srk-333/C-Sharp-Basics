dc7ff88662b747fbfadcc02864a1c066b05f5c66
srk-333 <sauravmk333@gmail.com>
2022-01-03 23:13:49 +0530

    [saurav] Added welcome Program

---
+++ HumanEntity/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumanEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO HUMAN");
+            Console.ReadLine();
+        }
+    }
+}
